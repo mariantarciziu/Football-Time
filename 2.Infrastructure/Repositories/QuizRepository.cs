24ed11c8cc61e018100bd2e7770cf41bcb918381
Marian Tarciziu <tarciziumarian@yahoo.com>
2019-10-30 08:26:36 +0200

    Take db from json

--- 2.Infrastructure/Repositories/QuizRepository.cs
+++ 2.Infrastructure/Repositories/QuizRepository.cs
@@ -7,31 +7,40 @@ using System.Linq.Expressions;
 using Core.Entities;
 using Core.Repository_Interfaces;
 using Core;
+using System.IO;
+using System.Reflection;
 
 namespace Infrastructure.Repositories
 {
     public class QuizRepository : IQuizRepository
     {
         private readonly FootballContext context;
+        private IEnumerable<Quiz> dbQuizzes; 
 
         public QuizRepository(FootballContext context)
         {
             this.context = context;
+            string _filePath = Path.GetDirectoryName(System.AppDomain.CurrentDomain.BaseDirectory);
+            _filePath = Directory.GetParent(Directory.GetParent(Directory.GetParent(_filePath).FullName).FullName).FullName +  @"\wwwroot\quizzes.json";
+            var content = File.ReadAllText(_filePath);
+            dbQuizzes = Newtonsoft.Json.JsonConvert.DeserializeObject<IEnumerable<Quiz>>(content);
         }
 
         public IQueryable<Quiz> GetAll()
         {
-            return context.Quizzes.Include(q=>q.Questions);
+            //return context.Quizzes.Include(q=>q.Questions);
+            return dbQuizzes.AsQueryable();
         }
 
         public IQueryable<Quiz> GetOnly(int number)
         {
-            return context.Quizzes.Include(q => q.Questions).OrderBy(quiz => Guid.NewGuid()).Take(number);
+            //return context.Quizzes.Include(q => q.Questions).OrderBy(quiz => Guid.NewGuid()).Take(number);
+            return dbQuizzes.OrderBy(quiz => Guid.NewGuid()).Take(number).AsQueryable();
         }
 
         public PagedList<Quiz> GetQuizzesForSpecificPage(int pageNumber, int pageSize)
         {
-            var quizzsBeforePagination = context.Quizzes.OrderByDescending(quiz => quiz.QuizId);
+            var quizzsBeforePagination = dbQuizzes.AsQueryable().OrderByDescending(quiz => quiz.Title);//context.Quizzes.OrderByDescending(quiz => quiz.QuizId);
             return PagedList<Quiz>.Create(quizzsBeforePagination, pageNumber, pageSize, quizzsBeforePagination.Count());
         }
 
